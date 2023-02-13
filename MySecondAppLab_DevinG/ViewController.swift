d8df291f5ccb71c58159a4837bcf75aaf61f0154
Dgriscow <dgrischow@students.mchenry.edu>
2023-02-13 10:56:42 -0600

    Initial Commit

---
+++ MySecondAppLab_DevinG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySecondAppLab_DevinG
+//
+//  Created by Devin Grischow on 2/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
